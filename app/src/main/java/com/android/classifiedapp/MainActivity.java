ad4b183760738727f40b92731cb2eac8deaf5198
ahsan-arif <ahsan.arifofficial@gmail.com>
2024-04-20 21:12:14 +0500

    added sign in with google

--- app/src/main/java/com/android/classifiedapp/MainActivity.java
+++ app/src/main/java/com/android/classifiedapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.android.classifiedapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,22 +15,40 @@ import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 
 //import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 TextView tvSignup;
+LinearLayout btnGoogleSignIn;
+    private static final int RC_SIGN_IN = 9001;
+    private ActivityResultLauncher<Intent> signInLauncher;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvSignup = findViewById(R.id.tv_signup);
+        btnGoogleSignIn = findViewById(R.id.btn_google_sign_in);
         FirebaseApp.initializeApp(getApplicationContext());
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //.requestIdToken(getString(R.string.google_client_id))
+                .requestEmail()
+                .build();
+        GoogleSignIn.getClient(this, gso);
         SpannableString spannableString = new SpannableString("Signup or login with email");
 
         // Create ClickableSpan for "Signup"
@@ -73,6 +93,36 @@ TextView tvSignup;
         tvSignup.setMovementMethod(LinkMovementMethod.getInstance());
         tvSignup.setText(spannableString);
 
+        signInLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                handleSignInResult(result.getData());
+            } else {
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+signIn(gso);
+            }
+        });
+
+    }
+    private void signIn(GoogleSignInOptions gso) {
+        Intent signInIntent = GoogleSignIn.getClient(this, gso).getSignInIntent();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void handleSignInResult(Intent data) {
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            Toast.makeText(this, "Sign in success: " + account.getEmail(), Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            Log.w(TAG, "signInResult:failed code=" + e.getMessage());
+            Toast.makeText(this, "Sign in failed "+e.getStatusCode(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
